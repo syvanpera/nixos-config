eff02a8aaa2609af54e463464b7592fed8e98f41
Tuomo Syvänperä <tuomo.syvanpera@gmail.com>
2023-04-20 23:07:06 +0300

    Add base configuration

--- flake.nix
+++ flake.nix
@@ -1,7 +1,18 @@
 {
-  description = "A very basic flake";
-  outputs = { self, nixpkgs }: {
-    package.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-    package.x86_64-linux.default = self.packages.x86_64-linux.hello;
+  description = "Tinimini's NixOS Config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+  };
+
+  outputs = { nixpkg, ... }@inputs: {
+    # NixOS configuration entrypoint. Available through
+    # 'nixos-rebuild --flake .#your-hostname'
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [ ./nixos/configuration.nix ];
+      };
+    };
   };
 }
