8a4a80e73c4a203e8223d6a18d3af2c74c80a0fb
Tuomo Syvänperä <tuomo.syvanpera@gmail.com>
2023-04-24 23:10:05 +0300

    Add devbox configuration

--- flake.nix
+++ flake.nix
@@ -5,13 +5,18 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
   };
 
-  outputs = { nixpkg, ... }@inputs: {
+  outputs = { nixpkgs, ... }@inputs: {
     # NixOS configuration entrypoint. Available through
     # 'nixos-rebuild --flake .#your-hostname'
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs; };
         modules = [ ./nixos/configuration.nix ];
+      };
+
+      devbox = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [ ./devbox/configuration.nix ];
       };
     };
   };
