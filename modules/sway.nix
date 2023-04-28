95075a56d5c4433ad4577644b3870b321b342307
Tuomo Syvänperä <tuomo.syvanpera@gmail.com>
2023-04-28 09:18:57 +0300

    Add dbus-sway-environment back

--- modules/sway.nix
+++ modules/sway.nix
@@ -1,5 +1,23 @@
 { inputs, config, lib, pkgs, ... }:
 let
+  # Bash script to let dbus know about important env variables and propagate them to relevent
+  # services run at the end of sway config
+  # See:
+  # https://github.com/emersion/xdg-desktop-portal-wlr/wiki/"It-doesn't-work"-Troubleshooting-Checklist
+  # Note: this is pretty much the same as  /etc/sway/config.d/nixos.conf but also restarts  
+  # some user services to make sure they have the correct environment variables
+  dbus-sway-environment = pkgs.writeTextFile {
+    name = "dbus-sway-environment";
+    destination = "/bin/dbus-sway-environment";
+    executable = true;
+
+    text = ''
+      dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
+      systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
+      systemctl --user start pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
+    '';
+  };
+
   # Currently, there is some friction between sway and gtk:
   # https://github.com/swaywm/sway/wiki/GTK-3-settings-on-Wayland
   #
